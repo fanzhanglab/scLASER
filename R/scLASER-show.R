d678aeae264bb40baf7ecbddaf615f836d71fe9b
Juan Vargas <82235338+juanvargas7@users.noreply.github.com>
2025-12-10 14:44:46 -0700

    update scLASER-show

--- R/scLASER-show.R
+++ R/scLASER-show.R
@@ -7,6 +7,14 @@
 setMethod("show", "scLASER", function(object) {
   cat("scLASER object\n")
   cat("---------------\n")
-
+  cat("Slots:")
+  cat("\t metadata: ",dim(metadata) )
+  cat("\t pcs: ",dim(pcs ))
+  cat("\t umap: ",dim(umap ))
+  cat("\t harmony: ",dim(harmony ))
+  cat("\t nam_pcs: ",dim(nam_pcs ))
+  cat("\t NAM_matrix: ",dim(NAM_matrix ))
+  cat("\t plsda_LV: ",dim(plsda_LV ))
+  cat("\t pipeline_output: ",dim(pipeline_output ))
 })
 
