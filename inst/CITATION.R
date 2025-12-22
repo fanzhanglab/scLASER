191344ddb5f202e1dbdb6e1cac51035a7e5cf0a1
Juan Vargas <82235338+juanvargas7@users.noreply.github.com>
2025-12-22 09:48:10 -0700

    create citation.R

---
+++ inst/CITATION.R
@@ -0,0 +1,20 @@
+citHeader("scLASER: Single-Cell Longitudinal Analysis, Simulation, and Visualization")
+
+citEntry(
+  entry = "Manual",
+  title = "scLASER: Single-Cell Longitudinal Analysis, Simulation, and Visualization",
+  author = personList(
+    person("Lauren", "Vanderlinden"),
+    person("Juan", "Vargas"),
+    person("Fan", "Zhang")
+  ),
+  year = "2025",
+  note = "R package version 0.1.0",
+  url = "https://github.com/fanzhanglab/scLASER",
+  textVersion = paste(
+    "Vanderlinden L, Vargas J, Zhang F (2025).",
+    "scLASER: Single-Cell Longitudinal Analysis, Simulation, and Visualization.",
+    "R package version 0.1.0.",
+    "https://github.com/fanzhanglab/scLASER"
+  )
+)
