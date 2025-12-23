07013aa15078888777923577d2b3c291cfd88a30
Juan Vargas <82235338+juanvargas7@users.noreply.github.com>
2025-12-22 17:58:27 -0700

    update tail count

--- R/tail_counts.R
+++ R/tail_counts.R
@@ -13,7 +13,7 @@
 #'
 #' @keywords internal
 #' @noRd
-tail_counts <- function(z, znull) {
+.tail_counts <- function(z, znull) {
   apply(znull, 2, function(znulli) {
     as.numeric(length(znulli) - cumsum(table(cut(znulli**2, c(0, z**2)))))
   })
