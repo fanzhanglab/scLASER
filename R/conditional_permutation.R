fc50ede342b80c8fdca9fa036fda1617d1a80c68
Juan Vargas <82235338+juanvargas7@users.noreply.github.com>
2025-12-22 17:58:41 -0700

    update conditional perm

--- R/conditional_permutation.R
+++ R/conditional_permutation.R
@@ -13,16 +13,15 @@
 #'
 #' @keywords internal
 #' @noRd
-conditional_permutation <- function(B, Y, num) {
+.conditional_permutation <- function(B, Y, num) {
   purrr::map(seq_len(num), function(i) {
     split(seq_len(length(Y)), B) %>%
-      purrr::map(function(idx) {
-        data.frame(idx, val = sample(Y[idx], replace = TRUE))
+      purrr::map(function(idx_vec) {
+        data.frame(idx = idx_vec, val = sample(Y[idx_vec], replace = TRUE))
       }) %>%
       dplyr::bind_rows() %>%
-      dplyr::arrange(idx) %>%
-      with(val)
+      dplyr::arrange(.data$idx) %>%
+      dplyr::pull(.data$val)
   }) %>%
     purrr::reduce(Matrix::cbind2)
 }
-
