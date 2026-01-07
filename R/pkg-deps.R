914a4f64aceb74c9657aa6928b25d25446224892
Juan Vargas <82235338+juanvargas7@users.noreply.github.com>
2026-01-07 13:56:09 -0700

    big dependencies changes

--- R/pkg-deps.R
+++ R/pkg-deps.R
@@ -5,18 +5,10 @@
 #' @import methods
 #' @import stats
 #' @import utils
-#' @import Matrix
-#' @import dplyr
-#' @import purrr
-#' @import MASS
-#' @import Seurat
-#' @import broom.mixed
-#' @import ggplot2
-#' @import harmony
-#' @import moments
-#' @import nlme
-#' @import uwot
-#' @import parallel
-#' @importFrom mixOmics plsda withinVariation
+#'
 #' @importFrom rlang .data
+#'
+#' @importFrom magrittr %>%
+#'
+#' @importFrom nlme lme lmeControl
 NULL
